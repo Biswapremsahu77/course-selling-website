649a434bb64e6f1d9994aa82bfda60a78fa893a4
OmB10 <ombamane10@gmail.com>
2023-11-06 12:10:12 +0530

    added all the models

---
+++ models/Course.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+
+const courseSchema = new mongoose.Schema({
+
+    courseName: {
+        type: String
+    },
+    CourseDescription: {
+        type: String,
+        trim: true,
+    },
+    instructor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    whatYouWillLearn: {
+        type: String
+    },
+    courseContent: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Section"
+        }
+    ],
+    ratingAndReviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RatingAndReviews"
+        }
+    ],
+    price: {
+        type: Number
+    },
+    thumbnail: {
+        type: String
+    },
+    tag: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tag"
+    },
+    studentsEnrolled: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        }
+    ]
+})
+
+module.exports = mongoose.model("Course", courseSchema)
