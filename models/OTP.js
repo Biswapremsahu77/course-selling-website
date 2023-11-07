492e09ef2ab35c70b60d06772af97ad24ab674f2
OmB10 <ombamane10@gmail.com>
2023-11-07 13:31:13 +0530

    added email verification function

--- models/OTP.js
+++ models/OTP.js
@@ -16,4 +16,22 @@ const OTPSchema = new mongoose.Schema({
     }
 })
 
+// function to send emails
+
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification Email from CourseHub", otp);
+        console.log("Email Sent Successfully: ", mailResponse);
+
+    } catch (error) {
+        console.log("error occured while sending mail: ", error);
+        throw error;
+    }
+}
+
+OTPSchema.pre("save", async function (next) {
+    await sendVerificationEmail(this.email, this.otp)
+    next();
+})
+
 module.exports = mongoose.model("OTP", OTPSchema)
